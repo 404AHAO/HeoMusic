2707e898ce0f571ee87ae0c8efa781c339c3718a
阿豪 <108629145+404AHAO@users.noreply.github.com>
2025-09-22 06:17:44 +0800

    Update and rename config.js.demo to config.js

--- config.js
+++ config.js
@@ -1,7 +1,15 @@
 var userId = "8668419170";
 var userServer = "tencent";
 var userType = "playlist";
-// var localMusic = [{
+var localMusic = [
+  {
+    name: 'Without You',
+    artist: 'Avicii、Sandro Cavazza',
+    url: 'https://image.ahao.ah.cn/Avicii%E3%80%81Sandro%20Cavazza%20-%20Without%20You.mp3',
+    cover: 'https://image.ahao.ah.cn/Avicii%E3%80%81Sandro%20Cavazza%20-%20Without%20You.jpg',
+    lrc: 'https://image.ahao.ah.cn/Avicii%E3%80%81Sandro%20Cavazza%20-%20Without%20You.lrc'
+},
+// {
 //     name: '重生之我在异乡为异客',
 //     artist: '王睿卓',
 //     url: '/music/重生之我在异乡为异客.mp3',
@@ -15,5 +23,5 @@ var userType = "playlist";
 //     cover: '/music/落.png',
 //     lrc: '/music/落.lrc'
 // }
-// ];
-// var remoteMusic = "./musiclist.json"
+];
+var remoteMusic = "./musiclist.json"
